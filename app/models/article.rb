cfe72e32ece2ccf5b7d3f451dd392e393aed45da
Ilya Shevchyk <il2001sh@gmail.com>
2021-05-21 23:36:50 +0300

    cascade comment deletion

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
   include Visible
-  has_many :comments
 
-  validates :title, presence: true, length: { minimum: 1 }
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
   validates :body, presence: true, length: { minimum: 1 }
 end
