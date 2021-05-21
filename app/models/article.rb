25d97a114996f479f1e0bb99a429b1fecf48385b
Ilya Shevchyk <il2001sh@gmail.com>
2021-05-21 23:11:09 +0300

    simplified view scope

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,7 @@
 class Article < ApplicationRecord
+  include Visible
   has_many :comments
 
   validates :title, presence: true, length: { minimum: 1 }
   validates :body, presence: true, length: { minimum: 1 }
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
