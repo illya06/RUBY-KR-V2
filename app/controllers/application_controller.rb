e7701c83a787db9469956af067cb00480883a4f7
Ilya Shevchyk <il2001sh@gmail.com>
2021-05-22 20:37:52 +0300

    validation added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     include Pundit
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+    private
+    def user_not_authorized
+        flash[:alert] = "You aint authorized man"
+        redirect_to(request.referrer || root_path)
+    end
 end
--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -1,9 +1,23 @@
 class ArticlePolicy < ApplicationPolicy
-    # def index?
-    #     true
-    # end
+    def index?
+        true
+    end
 
-    # def show?
-    #     true
-    # end
+    def show?
+        record.status == "public" || author?
+    end
+
+    def update?
+        author?
+    end
+
+    def destroy?
+        author?
+    end
+
+    private
+    def author?
+        return false unless user
+        record.author_id == user.id
+    end
 end
