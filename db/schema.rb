809fd28ddb40aa935be7f5acfc798244d49dfe03
Ilya Shevchyk <il2001sh@gmail.com>
2021-05-22 17:34:10 +0300

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_141744) do
+ActiveRecord::Schema.define(version: 2021_05_22_143306) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_05_22_141744) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -41,5 +43,6 @@ ActiveRecord::Schema.define(version: 2021_05_22_141744) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users", column: "author_id"
   add_foreign_key "comments", "articles"
 end
