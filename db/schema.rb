b6e018a7fefc3cf9d6998f2d6b38a91b1621bedc
Ilya Shevchyk <il2001sh@gmail.com>
2021-05-23 17:24:09 +0300

    added is_admin field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_145848) do
+ActiveRecord::Schema.define(version: 2021_05_23_141947) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_05_22_145848) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.boolean "is_admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
