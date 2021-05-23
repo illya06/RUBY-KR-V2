f675a9b21fd58642b868857b476fdc14ed3b167c
Ilya Shevchyk <il2001sh@gmail.com>
2021-05-23 18:18:56 +0300

    privete articles scope added

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,9 +6,11 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 1 }
 
-  scope :is_public, ->(val = true) { where(status: "public") }
+  scope :is_public, ->(val = true) { where(status: "public" || "archived") }
+  scope :is_private, ->(val = true) { where(status: "private") }
   scope :search, ->(request) {
     return if request.blank?
     where('title LIKE ?', "%#{request.squish}%")
   }
+
 end
