344a012f43ec3f23a75165a92100ddb2fd1dcaf9
Ilya Shevchyk <il2001sh@gmail.com>
2021-05-23 21:13:18 +0300

    added image_data to article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_141947) do
+ActiveRecord::Schema.define(version: 2021_05_23_181151) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_23_141947) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
     t.integer "author_id"
+    t.text "image_data"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
